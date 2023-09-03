2abbb8075fe791111221a919c23bd691650f0a50
H1ghPuR <varmasvse@gmail.com>
2023-09-03 00:26:38 -0500

    Main and side bar section initial commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@
 const App = () => {
   return (
     <div className="app">
-      <section className="side-bar"></section>
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history">
+          <li>howdy</li>
+        </ul>
+        <nav>
+          <p>ChatGPT Clone by Venkat Sagi</p>
+        </nav>
+      </section>
 
-      <section className="main"></section>
+      <section className="main">
+        
+      </section>
     </div>
   )
 }
