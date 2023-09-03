aa1045bc6ab79d50786052dd53f1f8dea1e9418f
H1ghPuR <varmasvse@gmail.com>
2023-09-03 02:45:36 -0500

    backend commit - getting back response from chatGPT API on console

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import {useState, useEffect} from 'react'
+
+
 
 const App = () => {
+
+  const [value, setValue] = useState(null)
+  const [message, setMessage] = useState(null)
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: "hello how are you?"
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+
+    try
+    {
+      const response = await fetch('http://localhost:8000/completions', options)
+      const data = await response.json()
+      console.log(data)
+      setMessage(data.choices[0].message)
+    }
+    catch (error)
+    {
+      console.error(error)
+    }
+  }
+
+
   return (
     <div className="app">
       <section className="side-bar">
@@ -20,7 +52,7 @@ const App = () => {
         <div className="bottom-section">
           <div className="input-container">
             <input/>
-            <div id="submit">➤</div>
+            <div id="submit" onClick={getMessages}>➤</div>
           </div>
           <p className="info">
             Free Research Preview. ChatGPT may produce 
